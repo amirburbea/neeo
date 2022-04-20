046bcfc6f8f11862061f1543f146268f2b942789
Amir Burbea <amir.burbea@gmail.com>
2022-04-19 20:29:45 -0400

    Precompute secure public key response.

--- Neeo.Sdk/Rest/Controllers/SecureController.cs
+++ Neeo.Sdk/Rest/Controllers/SecureController.cs
@@ -1,32 +1,14 @@
-﻿using System.IO;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Org.BouncyCastle.Bcpg;
-using Org.BouncyCastle.Bcpg.OpenPgp;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Neeo.Sdk.Rest.Controllers;
 
 [ApiController, Route("[controller]")]
 internal sealed class SecureController : ControllerBase
 {
-    private readonly PgpPublicKey _publicKey;
+    private readonly PublicKeyResponse _publicKey;
 
-    public SecureController(PgpKeyPair pgpKeys) => this._publicKey = pgpKeys.PublicKey;
+    public SecureController(PublicKeyResponse publicKey) => this._publicKey = publicKey;
 
     [HttpGet("pubkey")]
-    public async Task<ActionResult<PublicKeyResponse>> GetPublicKey()
-    {
-        using MemoryStream outputStream = new();
-        using (ArmoredOutputStream armoredStream = new(outputStream))
-        {
-            armoredStream.SetHeader(ArmoredOutputStream.HeaderVersion, default);
-            this._publicKey.Encode(armoredStream);
-        }
-        outputStream.Seek(0L, SeekOrigin.Begin);
-        using StreamReader reader = new(outputStream);
-        return new PublicKeyResponse(await reader.ReadToEndAsync());
-    }
-
-    public record struct PublicKeyResponse([property: JsonPropertyName("publickey")] string PublicKey);
+    public ActionResult<PublicKeyResponse> GetPublicKey() => this._publicKey;
 }
---
+++ Neeo.Sdk/Rest/PublicKeyResponse.cs
@@ -0,0 +1,5 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Neeo.Sdk.Rest;
+
+public sealed record class PublicKeyResponse([property: JsonPropertyName("publickey")] string PublicKey);
